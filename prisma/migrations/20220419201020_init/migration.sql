6a91b7ca6b95b6ca82a7f3e633c67a0b605e2ab7
Thiago Magano <thiagomagano1993@outlook.com>
2022-04-19 18:01:45 -0300

    change schema and add some endpoints

--- prisma/migrations/20220419201020_init/migration.sql
+++ prisma/migrations/20220419201020_init/migration.sql
@@ -2,10 +2,19 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
     "name" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Pokemon" (
+    "id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" TEXT[],
+    "avatarUrl" TEXT NOT NULL,
+
+    CONSTRAINT "Pokemon_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -15,19 +24,9 @@ CREATE TABLE "Party" (
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "title" VARCHAR(255) NOT NULL,
     "userId" INTEGER NOT NULL,
+    "pokemonsIds" INTEGER[],
 
     CONSTRAINT "Party_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Pokemon" (
-    "id" INTEGER NOT NULL,
-    "name" TEXT NOT NULL,
-    "type" TEXT[],
-    "avatarUrl" TEXT NOT NULL,
-    "partyId" INTEGER,
-
-    CONSTRAINT "Pokemon_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -35,6 +34,3 @@ CREATE UNIQUE INDEX "Party_userId_key" ON "Party"("userId");
 
 -- AddForeignKey
 ALTER TABLE "Party" ADD CONSTRAINT "Party_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Pokemon" ADD CONSTRAINT "Pokemon_partyId_fkey" FOREIGN KEY ("partyId") REFERENCES "Party"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,71 @@
 import express from 'express'
 import cors from 'cors'
-// Porta do servidor
-const PORT = process.env.PORT || 4000
-// Host do servidor
-const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
-// App Express
+import { Prisma, PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 const app = express()
+
+
+app.use(express.json())
+
+// Porta do servidor
+const PORT = process.env.PORT || 3333
+
 // Endpoint raiz
 app.get('/', (req, res) => {
-    res.send('Bem-vindo!')
+    res.send('Bem-vindo, cria!')
 })
-// Cors
-app.use(cors({
-    origin: ['http://localhost:3000']
-}))
+
 // Resposta padrão para quaisquer outras requisições:
 app.use((req, res) => {
     res.status(404)
 })
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+
+//cia um novo usuário
+// app.post('/signup', async (req, res) => {
+//   const { name, email, partys } = req.body
+//   console.log(partys)
+
+//   const partyData = partys?.map((party: Prisma.PartyCreateInput) => {
+//     return {tittle: party?.title, pokemonsIds: party?.pokemonsIds}
+//   })
+
+//     const result = await prisma.user.create({
+//       data: {
+//         name,
+//         email,
+//         partys: partyData
+//       },
+//     }).catch(err => console.log(err))
+    
+//   res.json(result)
+// })
+
+app.post('/pokemon', async (req, res) => {
+  const { id, name, type, avatarUrl } = req.body
+  
+  try {
+    const result = await prisma.pokemon.create({
+      data: {
+        id,
+        name,
+        type,
+        avatarUrl
+      }
+    })
+      res.json(result)
+  } catch (err) {
+    res.json({ error: err })
+  }
+})
+
+
 // Inicia o sevidor
 app.listen(PORT, () => {
-    console.log(`Servidor rodando com sucesso ${HOSTNAME}:${PORT}`)
+    console.log(`Servidor rodando com sucesso http://localhost:${PORT}`)
 })
